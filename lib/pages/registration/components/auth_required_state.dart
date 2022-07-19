22dc143bc1f729bc2d7a94b4b512d53926fab721
f1rrock <flyrock000@gmail.com>
2022-07-19 14:03:30 +0300

    add auth require

--- lib/pages/registration/components/auth_required_state.dart
+++ lib/pages/registration/components/auth_required_state.dart
@@ -4,39 +4,14 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class AuthRequiredState < T extends StatefulWidget >
     extends SupabaseAuthRequiredState < T > {
 
-  /*@override
-  void onAuthenticated ( ) {
-
-    /// Users will be sent back to the LoginPage if they sign out.
-    if ( mounted ) {
-
-      print('unauth required mount');
-
-      /// Users will be sent back to the LoginPage if they sign out.
-      Navigator.of ( context ).pushNamedAndRemoveUntil ( '/hello', ( route ) => false );
-
-    } else {
-
-      print('unauth required not mount');
-
-    }
-
-  }*/
-
   @override
   void onUnauthenticated ( ) {
 
     /// Users will be sent back to the LoginPage if they sign out.
     if ( mounted ) {
 
-      print('unauth required mount');
-
       /// Users will be sent back to the LoginPage if they sign out.
       Navigator.of ( context ).pushNamedAndRemoveUntil ( '/confirmEmail', ( route ) => false );
-
-    } else {
-
-      print('unauth required not mount');
 
     }
 
